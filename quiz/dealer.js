0da95112b065129831f1ec57fba87b40778786b0
sotatuka <9999qweasdrf@gmail.com>
2026-07-27 19:04:23 +0900

    commit

---
+++ quiz/dealer.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", e => {
+    const body =  document.querySelector("main");//"body|main"でも可.
+    const rootDomDealer = new Deal(body);
+    /*
+    rootDomDealer.CE("div", { id: "box", class: "main" });
+    rootDomDealer.CCE("#box", "p", { text: "Paragraph", id: "para1" });
+    rootDomDealer.CCE("#box", "p", { text: "Paragraph 2", id: "para2" });
+    rootDomDealer.CCE("#box|#para1", "div", { text: "para1div", id: "para1div"})
+    const data = rootDomDealer.GET("#para1");
+    const data2 = rootDomDealer.GET("#para1|div");
+    rootDomDealer.CCE("#box","p", { text: JSON.stringify(data), id: "dated"})
+    rootDomDealer.CCE("#box","p", { text: JSON.stringify(data2), id: "dated2"})
+    */
+    //ここは例だからコメントアウト.
+
+    //rootDomDealer.MODIFY()書いてないけどとにかく横並び.
+    
+    //上記のようにしたかったけど構造上できない。これだからすべての要素を一から作ったほうがいいんですね.
+    rootDomDealer.CE("div", { id: "block", styles: { display: "flex", height: "100vh", margin: "0", padding: "0", "background-color": "black"}})
+    rootDomDealer.CCE("#block", "div", { id: "leftblock", styles: { width: "50%", height: "100vh", "background-color": "black"}});
+    rootDomDealer.CCE("#block", "div", { id: "rightblock", styles: { width: "50%","margin-top": "40px", "margin-bottom": "80px", "margin-right": "20px", "margin-left": "20px" , border : "solid 5px grey", "border-radius": "15px", "background-color": "black"}});
+    function createQuizUI(data) {
+
+    }
+    function getquizdata() {
+        fetch("https://github.tacz.f5.si/quizData/1.json").then(
+            res => res.json()
+        ).then(
+            data => {
+                alert("yes" + JSON.stringify(data))
+            }
+        ).catch(error => {
+            alert("no" + JSON.stringify(error))
+        })
+    }
+    getquizdata();
+})
