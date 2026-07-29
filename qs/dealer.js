0556bbdd5d99bf52cdf5cbf4423a63de7c7bb28c
sotatuka <9999qweasdrf@gmail.com>
2026-07-29 16:28:56 +0900

    commit

--- qs/dealer.js
+++ qs/dealer.js
@@ -2,8 +2,8 @@ function initializeQuiz() {
     const body = document.querySelector("main");
     const rootDomDealer = new Deal(body);
     
-    rootDomDealer.CE("div", { id: "header", styles: { height: "15vh", margin: "0", padding: "0", "background-color": "#333333"}})
-    rootDomDealer.CE("div", { id: "block", styles: { display: "flex", height: "85vh", "box-sizing": "border-box", margin: "0", padding: "0", "background-color": "black"}})
+    rootDomDealer.CE("div", { id: "header", styles: { height: "8vh", margin: "0", padding: "0", "background-color": "#333333"}})
+    rootDomDealer.CE("div", { id: "block", styles: { display: "flex", height: "92vh", "box-sizing": "border-box", margin: "0", padding: "0", "background-color": "black"}})
     rootDomDealer.CCE("#block", "div", { id: "leftblock", styles: { width: "50%", height: "100%", "background-color": "black"}});
     rootDomDealer.CCE("#block", "div", { id: "rightblock", styles: { width: "50%", height: "100%", "box-sizing": "border-box", "background-color": "black", padding: "40px 20px 80px 20px"}});
     rootDomDealer.CCE("#block|#rightblock", "div", { id: "rightcontent", styles: { height: "100%", "box-sizing": "border-box", border : "solid 5px #333333", "border-radius": "15px", "background-color": "#000000"}})
