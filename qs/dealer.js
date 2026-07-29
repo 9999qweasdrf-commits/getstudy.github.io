6ec106338f6c6c04bfbfb431712d23911fb48f5d
sotatuka <9999qweasdrf@gmail.com>
2026-07-29 16:20:09 +0900

    commi

--- qs/dealer.js
+++ qs/dealer.js
@@ -29,7 +29,7 @@ function initializeQuiz() {
     const DATA = getquizdata();
 }
 
-// ページが既に読み込まれている場合は即座に実
+// ページが既に読み込まれている場合は即座に実a
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', initializeQuiz);
 } else {
